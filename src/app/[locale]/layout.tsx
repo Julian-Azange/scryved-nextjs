024d1cc4a04c30839928577b7901e8abf565529f
JulianPDP <julianazange@gmail.com>
2026-05-08 19:59:21 -0500

    feat: preloader

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import SmoothScroll from "@/src/components/layout/SmoothScroll";
+import Preloader from "@/src/components/ui/Preloader";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -19,23 +20,27 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-  params, // 1. Recibimos params sin desestructurar
+  params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ locale: string }>; // 2. Tipamos params como una Promesa
+  params: Promise<{ locale: string }>;
 }) {
-  // 3. Resolvemos la promesa antes de usarla
   const { locale } = await params;
-
   const messages = await getMessages();
 
   return (
     <html lang={locale}>
       <body className={`${poppins.variable} font-sans antialiased bg-black text-white`}>
         <NextIntlClientProvider messages={messages}>
+
+          {/* 1. Ponemos el Preloader aquí arriba */}
+          <Preloader />
+
+          {/* 2. El resto de la app envuelta en SmoothScroll */}
           <SmoothScroll>
             {children}
           </SmoothScroll>
+
         </NextIntlClientProvider>
       </body>
     </html>
